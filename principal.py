6bb738c7c5f0d5b626a08df9ff465f50c3509cb3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-03 20:36:37 -0300

    Criação da função que exibe o menu.

---
+++ principal.py
@@ -0,0 +1,10 @@
+def exibeMenu():
+    print('Bem-vindo(a) ao sistema escolar "Arte de Educar".')
+    print('Opção 1 - Cadastrar aluno.')
+    print('Opção 2 - Cadastrar nota.')
+    print('Opção 3 - Consultar aluno.')
+    print('Opção 4 - Cadastrar professor.')
+    print('Opção 5 - Consultar professor.')
+    print('Opção 6 - Sair.')
+
+exibeMenu()

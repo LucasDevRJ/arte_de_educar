0bda5b30b066411cd86bfff9f71687600a77340f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-03 20:50:57 -0300

    Tratamento de valor digitado pelo usuário.

--- principal.py
+++ principal.py
@@ -1,10 +1,20 @@
 def exibeMenu():
-    print('Bem-vindo(a) ao sistema escolar "Arte de Educar".')
-    print('Opção 1 - Cadastrar aluno.')
-    print('Opção 2 - Cadastrar nota.')
-    print('Opção 3 - Consultar aluno.')
-    print('Opção 4 - Cadastrar professor.')
-    print('Opção 5 - Consultar professor.')
-    print('Opção 6 - Sair.')
+    while True:
+        print('-' * 30, '|MENU PRINCIPAL|', '-' * 30)
+        print('Bem-vindo(a) ao sistema escolar "Arte de Educar".')
+        print('Opção 1 - Cadastrar aluno.')
+        print('Opção 2 - Cadastrar nota.')
+        print('Opção 3 - Consultar aluno.')
+        print('Opção 4 - Cadastrar professor.')
+        print('Opção 5 - Consultar professor.')
+        print('Opção 6 - Sair.')
+        print('-' * 79)
+        try:
+            opcao = int(input('Digite sua opção desejada: '))
+            print()
+        except ValueError:
+            print('Dígito incorreto!\nDigite somente as opções presentes.')
+            print()
+            continue
 
 exibeMenu()

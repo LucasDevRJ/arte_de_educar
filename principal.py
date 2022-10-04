1929684b66ff2738a74a38ff6e43aada5076a82e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-04 16:05:22 -0300

    Adição do atributo matrícula na função de cadastrar aluno.

--- principal.py
+++ principal.py
@@ -1,58 +1,59 @@
-def cadastrarAluno():
+contadoraMatricula = 100
+
+
+def cadastrarAluno(matricula):
     print('-' * 30, '|CADASTRAR ALUNO|', '-' * 30)
 
     alunos = []
+    matricula += 1
 
-    quantidadeAlunos = int(input('Digite a quantidade de alunos que deseja cadastrar: '))
+    nome = str(input('Digite o nome: '))
 
-    for i in range(0, quantidadeAlunos, 1):
+    while not nome.isalpha():
+        print('Dígito incorreto!\nDigite somente letras e acentos.')
         nome = str(input('Digite o nome: '))
 
-        while not nome.isalpha():
-            print('Dígito incorreto!\nDigite somente letras e acentos.')
-            nome = str(input('Digite o nome: '))
+    sobrenome = str(input('Digite o sobrenome: '))
 
+    while not sobrenome.isalpha():
+        print('Dígito incorreto!\nDigite somente letras e acentos.')
         sobrenome = str(input('Digite o sobrenome: '))
 
-        while not sobrenome.isalpha():
-            print('Dígito incorreto!\nDigite somente letras e acentos.')
-            sobrenome = str(input('Digite o sobrenome: '))
+    dataNascimento = str(input('Digite a data de nascimento: '))
 
+    while not dataNascimento.isdigit():
+        print('Dígito incorreto!\nDigite números e caracteres matemáticos.')
         dataNascimento = str(input('Digite a data de nascimento: '))
 
-        while not dataNascimento.isdigit():
-            print('Dígito incorreto!\nDigite números e caracteres matemáticos.')
-            dataNascimento = str(input('Digite a data de nascimento: '))
+    while True:
+        try:
+            tipoEnsino = int(input('Escolha a classficação de ensino\n'
+                                   'Opção 1 - Ensino Fundamental\nOpção 2 - Ensino Médio\nDigite: '))
+            if tipoEnsino == 1 or tipoEnsino == 2:
+                break
+        except ValueError:
+            print('Dígito incorreto!\nDigite somente as opções válidas.')
+            continue
 
-        while True:
-            try:
-                tipoEnsino = int(input('Escolha a classficação de ensino\n'
-                                       'Opção 1 - Ensino Fundamental\nOpção 2 - Ensino Médio\nDigite: '))
-                if tipoEnsino == 1 or tipoEnsino == 2:
-                    break
-            except ValueError:
-                print('Dígito incorreto!\nDigite somente as opções válidas.')
-                continue
+    genero = str(input('Digite o gênero: '))
 
+    while not genero == 'M' and genero == 'm' and genero == 'F' \
+            or genero == 'f' and genero == 'O' and genero == 'o':
+        print('Gênero inválido!\nDigite somente M, F ou O.')
         genero = str(input('Digite o gênero: '))
 
-        while not genero == 'M' and genero == 'm' and genero == 'F' \
-                or genero == 'f' and genero == 'O' and genero == 'o':
-            print('Gênero inválido!\nDigite somente M, F ou O.')
-            genero = str(input('Digite o gênero: '))
+    if tipoEnsino == 2:
+        tipoEnsino = 'Ensino Médio'
+    else:
+        tipoEnsino = 'Ensino Fundamental'
 
-        if tipoEnsino == 2:
-            tipoEnsino = 'Ensino Médio'
-        else:
-            tipoEnsino = 'Ensino Fundamental'
+    nome = nome.upper()
+    sobrenome = sobrenome.upper()
+    genero = genero.upper()
+    tipoEnsino = tipoEnsino.upper()
 
-        nome = nome.upper()
-        sobrenome = sobrenome.upper()
-        genero = genero.upper()
-        tipoEnsino = tipoEnsino.upper()
+    alunos.append([nome, matricula, sobrenome, dataNascimento, tipoEnsino, genero])
 
-        alunos.append([nome, sobrenome, dataNascimento, tipoEnsino, genero])
-        print(alunos)
 
 def exibeMenu():
     while True:
@@ -70,7 +71,7 @@ def exibeMenu():
             print()
 
             if opcao == 1:
-                cadastrarAluno()
+                cadastrarAluno(contadoraMatricula)
 
         except ValueError:
             print('Dígito incorreto!\nDigite somente as opções presentes.')

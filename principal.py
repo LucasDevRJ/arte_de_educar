094c4eba1c7505e89c51d3a11d3bd102590cd3ca
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-03 21:12:42 -0300

    Criação de listas e entradas de dados.

--- principal.py
+++ principal.py
@@ -1,6 +1,23 @@
 def cadastrarAluno():
     print('-' * 30, '|CADASTRAR ALUNO|', '-' * 30)
 
+    alunos = []
+    aluno = []
+
+    quantidadeAlunos = int(input('Digite a quantidade de alunos que deseja cadastrar: '))
+
+    for i in range(0, quantidadeAlunos, 1):
+        nome = str(input('Digite o nome: '))
+        sobrenome = str(input('Digite o sobrenome: '))
+        dataNascimento = str(input('Digite a data de nascimento: '))
+        tipoEnsino = int(input('Opção 1 - Ensino Fundamental\n2 - Ensino Médio\nDigite: '))
+        genero = str(input('Digite o gênero: '))
+
+        aluno.append(nome)
+        aluno.append(sobrenome)
+        aluno.append(dataNascimento)
+        aluno.append(tipoEnsino)
+        aluno.append(genero)
 
 def exibeMenu():
     while True:
